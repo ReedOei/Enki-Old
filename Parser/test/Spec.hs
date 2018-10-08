b552641119c142da84343059352ee306cc4c0b4d
Reed Oei <oei.reed@gmail.com>
2018-10-08 10:30:40 -0500

    Added some dependencies for the haskell project

--- Parser/test/Spec.hs
+++ Parser/test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Test.QuickCheck
+
+reverseTestProp :: [Integer] -> Bool
+reverseTestProp xs = reverse (reverse xs) == xs
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "quickcheck_example" $ do
+        it "reverses lists properly" $ property reverseTestProp
+
